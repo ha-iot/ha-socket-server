8a14fdd4d7301bbe9bf4bbb7acfb086c1f59ff8f
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-02-14 21:58:30 -0800

    :art: Split code in functions

--- app/client/listeners.js
+++ app/client/listeners.js
@@ -2,38 +2,38 @@ const {END_USER_ROOM, HARDWARE_ACTIONS} = require('../constants')
 
 module.exports = (io, socket, state) => {
   socket.join(END_USER_ROOM)
-  _emitClientsLogged(io)
+  _emitClientsLogged()
 
   socket.on('client/getLampsState', () => {
     socket.emit('client/lampsState', state.lamps)
   })
 
-  socket.on('client/lampsAction',
-    /**
-     * @param {{target, action}} data
-     */
-    (data) => {
-      if (!state.hardwareHandler) {
-        socket.emit('client/response', {message: 'No handler connected.'})
-        return
-      }
-
-      if (data.target && HARDWARE_ACTIONS.has(data.action)) {
-        state.hardwareHandler.emit('hardware/action', data)
-      } else {
-        socket.emit('client/response', {message: 'You sent invalid data.'})
-      }
-    }
-  )
+  socket.on('client/lampsAction', _lampsAction)
 
   socket.on('disconnect', () => {
     socket.leave(END_USER_ROOM)
-    _emitClientsLogged(io)
+    _emitClientsLogged()
   })
-}
 
-function _emitClientsLogged (io) {
-  io.of('/').in(END_USER_ROOM).clients((_, clients) => {
-    io.to(END_USER_ROOM).emit('client/clientsConnected', {data: clients.length})
-  })
+  /**
+   * @param {{target, action}} data
+   */
+  function _lampsAction (data) {
+    if (!state.hardwareHandler) {
+      socket.emit('client/response', {message: 'No handler connected.'})
+      return
+    }
+
+    if (data.target && HARDWARE_ACTIONS.has(data.action)) {
+      state.hardwareHandler.emit('hardware/action', data)
+    } else {
+      socket.emit('client/response', {message: 'You sent invalid data.'})
+    }
+  }
+
+  function _emitClientsLogged () {
+    io.of('/').in(END_USER_ROOM).clients((_, clients) => {
+      io.to(END_USER_ROOM).emit('client/clientsConnected', {data: clients.length})
+    })
+  }
 }
