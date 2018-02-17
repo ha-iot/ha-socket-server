892226f3859308ba49457ca1b4690b8fc7f2d0fb
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-02-16 23:17:41 -0800

    :snowflake: Do not load the database during tests

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config({silent: true})
 
-require('./models')
+const TEST = process.argv.some(arg => /test/.test(arg))
+
+if (!TEST) {
+  // Hitting the database during tests was causing it to take forever (literally)
+  require('./models')
+}
 
 const server = require('http').Server(require('express')())
 const PORT = +process.env.PORT || 3003
