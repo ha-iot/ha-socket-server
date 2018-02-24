1684ceee27a63690333bc2233379b1c560a85d97
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-02-24 11:34:55 -0800

    :art: Change board listeners structure

--- app/board/listeners.js
+++ app/board/listeners.js
@@ -4,29 +4,32 @@ module.exports = (io, socket, state) => {
   state.hardwareHandler = socket
   socket.emit('hardware/getData')
 
-  socket.on('hardware/data',
-    /**
-     * @param {{lampsState, hardwareActions}} data
-     */
-    (data) => {
-      data.hardwareActions.forEach(action => {
-        HARDWARE_ACTIONS.add(action)
-      })
-      emitLampsToClient(data.lampsState)
-    }
-  )
+  const _this = {io, state}
 
-  socket.on('hardware/lampsState', lamps => {
-    emitLampsToClient(lamps)
+  socket.on('hardware/data', _receiveBoardData.bind(_this))
+  socket.on('hardware/lampsState', _receiveBoardLampsState.bind(_this))
+  socket.on('disconnect', _disconnect.bind(_this))
+}
+
+function _disconnect () {
+  const {io, state} = this
+  HARDWARE_ACTIONS.clear()
+  state.hardwareHandler = null
+  io.to(END_USER_ROOM).emit('client/lampsState', state.lamps = [])
+}
+
+/**
+ * @param {{lampsState, hardwareActions}} data
+ */
+function _receiveBoardData (data) {
+  const {io, state} = this
+  data.hardwareActions.forEach(action => {
+    HARDWARE_ACTIONS.add(action)
   })
+  io.to(END_USER_ROOM).emit('client/lampsState', state.lamps = data.lampsState)
+}
 
-  socket.on('disconnect', () => {
-    HARDWARE_ACTIONS.clear()
-    state.hardwareHandler = null
-    emitLampsToClient([])
-  })
-
-  function emitLampsToClient (lamps) {
-    io.to(END_USER_ROOM).emit('client/lampsState', state.lamps = lamps)
-  }
+function _receiveBoardLampsState (lamps) {
+  const {io, state} = this
+  io.to(END_USER_ROOM).emit('client/lampsState', state.lamps = lamps)
 }
