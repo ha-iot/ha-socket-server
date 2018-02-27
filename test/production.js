4d322106a1407a8ee4cf07671c0fbe73087c8a5d
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-02-26 22:57:32 -0800

    :art: Improve production test code

--- test/production.js
+++ test/production.js
@@ -1,22 +1,21 @@
+const http = require('http')
 const path = require('path')
 
 const indexPath = path.resolve(path.dirname(__dirname), 'index.js')
 
 describe('Production server', () => {
-  beforeEach(() => {
-    delete require.cache[indexPath]
-  })
-
   it('must start in the default port', done => {
     process.env.PORT = undefined
     const PORT = 3003
     const server = require(indexPath)
+    delete require.cache[indexPath]
 
     const _oldLog = console.log
     console.log = () => undefined
 
-    require('http').get(`http://localhost:${PORT}`, response => {
+    http.get(`http://localhost:${PORT}`, response => {
       response.statusCode.should.be.equal(404)
+
       server.close()
       console.log = _oldLog
       done()
@@ -27,12 +26,14 @@ describe('Production server', () => {
     const PORT = 9834
     process.env.PORT = PORT.toString()
     const server = require(indexPath)
+    delete require.cache[indexPath]
 
     const _oldLog = console.log
     console.log = () => undefined
 
-    require('http').get(`http://localhost:${PORT}`, response => {
+    http.get(`http://localhost:${PORT}`, response => {
       response.statusCode.should.be.equal(404)
+
       server.close()
       console.log = _oldLog
       done()
